5a961ca5aae714c0821a0d8f87420ff66c74d2cb
Shashank reddy Bobbala <shashankreddy444@gmail.com>
2024-02-03 17:16:00 -0600

    added linked list

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -1,37 +1,86 @@
 class LinkedList {
-    constructor(value){
+    constructor(value) {
         this.head = {
-           value: value,
-           next:null
+            value: value,
+            next: null
         }
         this.tail = this.head;
         this.length = 1
     }
 
-    append(value){
-     let newNode = {
-        value: value,
-        next:null
-     }
- 
-     this.tail.next = newNode;
-     this.tail = newNode;
-     this.length++
-     return this
-    }
-
-    prepend(value){
+    append(value) {
         let newNode = {
             value: value,
-            next:null
-         } 
+            next: null
+        }
 
-         newNode.next = this.head;
-         this.head = newNode
-         this.length++;
-         return this;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++
+        return this
+    }
+
+    prepend(value) {
+        let newNode = {
+            value: value,
+            next: null
+        }
+
+        newNode.next = this.head;
+        this.head = newNode
+        this.length++;
+        return this;
+    }
+
+    insert(index, value) {
+        let newNode = {
+            value: value,
+            next: null
+        }
+
+        if (index === 0 || index < 0) {
+            this.prepend(value)
+        } else if (index > this.length) {
+            this.append(value)
+        } else {
+
+            let list = this.head
+            let current = 0;
+            while (current < index - 1) {
+                list = list.next;
+                current++
+            }
+            let addValue = list;
+            let header = list.next
+            addValue.next = newNode;
+            addValue.next.next = header;
+            this.length++
+            return this
+        }
+
+    }
+
+    remove(index, value) {
+
+        let newNode = {
+            value: value,
+            next: null
+        }
+
+        let current = 0;
+        let list = this.head;
+        while (current < index - 1) {
+            list = list.next;
+            current++
+        }
+        let removeNode = list;
+        let header = list.next;
+        removeNode.next = header.next;
+        this.length--
     }
 }
+
+
 
 let linkedList = new LinkedList(10)
 linkedList.append(11)
@@ -39,5 +88,10 @@ linkedList.append(12)
 console.log(linkedList.append(13))
 linkedList.prepend(9)
 console.log(linkedList.prepend(8))
+
+console.log(linkedList.insert(3, 10.5))
+console.log(linkedList.insert(1, 8.5))
+console.log(linkedList.insert(20, 14))
+
 
 
