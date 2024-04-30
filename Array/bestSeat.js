3ff64ce471bc9983a0e71f5d69459d2b8657d248
Shashank reddy Bobbala <shashankreddy444@gmail.com>
2024-04-30 13:55:55 -0500

    bestseat

--- Array/bestSeat.js
+++ Array/bestSeat.js
@@ -1,21 +1,35 @@
-function bestSeat(seats) {
 
-    let bestSeat = -1;
-    let maxSpace = 0;
-    let left = 0;
-    while (left < seats.length) {
-        right = left + 1;
-        while (right < seats.length && seats[right] === 0) {
-            right += 1
-        }
+// in array you have to find best seat, 0's is empty seat and 1's filled. first and last always filled in array with 1's.
+//   You may assume that someone is always sitting in the
+//   first and last seat of the row. Whenever there are two equally good seats,
+//   you should sit in the seat with the lower index. If there is no seat to sit
+//   in, return -1. The given array will always have a length of at least one
+//   and contain only ones and zeroes.
 
-        avalaibleSpace = right - left - 1;
-        bestSeat = Math.floor((left + right) / 2);
-        maxSpace = avalaibleSpace;
-
-    }
-    left = right
-
+function bestSeat(arr){
+  let bestSeat = -1;
+  let left = 0;
+  let maxSpace = 0;
+  
+  while(left < arr.length){
+    right = left+1;
+  
+while(right<arr.length && arr[right]===0){
+  right++;
 }
 
+let availableSpace = right -left -1;
+if(availableSpace > maxSpace){
+  bestSeat = Math.floor((left+right)/2);
+  maxSpace = availableSpace
+}
+
+left = right
+
+
+}
+return bestSeat
+}
+console.log(bestSeat([1, 0, 1, 0, 0, 0, 1]))
+console.log(bestSeat([ 1]))
 
